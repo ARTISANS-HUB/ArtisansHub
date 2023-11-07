ccaaf2bec874767df2bc101761a13ccfd7abd3f1
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2023-11-07 14:01:15 +0000

    responsive done

--- client/src/views/platform/index.js
+++ client/src/views/platform/index.js
@@ -4,11 +4,11 @@ import "../../css/platform.css";
 import Banner from "./includes/Banner";
 import ServiceCategory from "./includes/ServiceCategory";
 import FilterCategory from "./includes/FilterCategory";
-
+import TopNav from "./includes/topNavBar"
 const Index = () => {
 	return (
 		<div>
-	
+		<TopNav/>
 
 		<Banner/>
 		<ServiceCategory/>
