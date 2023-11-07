989937396e602c7a8a921f3989936833f9fa7b14
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2023-11-07 14:23:20 +0000

    responsive nav done

--- client/src/views/platform/index.js
+++ client/src/views/platform/index.js
@@ -4,14 +4,6 @@ import "../../css/platform.css";
 import Banner from "./includes/Banner";
 import ServiceCategory from "./includes/ServiceCategory";
 import FilterCategory from "./includes/FilterCategory";
-<<<<<<< HEAD
-import TopNav from "./includes/topNavBar"
-const Index = () => {
-	return (
-		<div>
-		<TopNav/>
-
-=======
 import ArtisansCard from "./includes/ArtisansCard";
 
 const Index = () => {
@@ -19,7 +11,6 @@ const Index = () => {
 		<div>
 	
 		<TopNavBar/>
->>>>>>> c702fbde9e71d04f3fa62c0e6ec479628eb204a0
 		<Banner/>
 		<ServiceCategory/>
 		<ArtisansCard/>
