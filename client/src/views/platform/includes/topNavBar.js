20af557e01799ccb47d47a463fbe72ce26699e3d
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2023-11-07 12:55:44 +0100

    Update topNavBar.js

--- client/src/views/platform/includes/topNavBar.js
+++ client/src/views/platform/includes/topNavBar.js
@@ -1,30 +1,66 @@
 import { Link } from "react-router-dom";
+import "../../../css/createBuyer.css";
+import "../../../css/topNavBar.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBell} from "@fortawesome/free-solid-svg-icons"
 
 const TopNavBar = () => {
+  const handleChange = async (event) => {
+    event.preventDefault();
+
+    if (event.target.value === "buyer") {
+      window.location.href = "/create-buyer";
+    } else if (event.target.value === "artisan") {
+      window.location.href = "/create-artisan";
+    } else if (event.target.value === "admin") {
+      window.location.href = "/login";
+    }
+  };
+
+
   return (
     <nav>
       <div className="TopNavBar-platform">
         <div className="TopNavBar-platform-inner">
-          <div>
-            <div>
-              <Link>DigiHave</Link>
+          <div className="logo">
+            <Link>DiGiHaven</Link>
+          </div>
+          <div className="search-bar ">
+            <input type="text" placeholder="search" />
+          </div>
+
+          <div className="navbar">
+            <div className="contact">
+              <i>Contact</i>
             </div>
             <div>
-              <input type="text" name="" id="" placeholder="Search" />
+              <select id="selectOption" onChange={handleChange} className="become-button">
+                <option value="" selected disabled>
+                  <Link to="#">
+                    <i> Become</i>
+                  </Link>
+                </option>
+                <option value="buyer">
+                  <Link to="/become-buyer">
+                    <i> Buyer</i>
+                  </Link>
+                </option>
+                <option value="artisan">
+                  <Link to="/become-artisan">
+                    <i> Artisan</i>
+                  </Link>
+                </option>
+
+                <option value="admin">
+                  <Link to="#">
+                    <i> Admin Login </i>
+                  </Link>
+                </option>
+              </select>
             </div>
-            <div>
-              <div>
-                <h2>Contact Us</h2>
+            <div className="notification">
+                  <FontAwesomeIcon icon={faBell}/>
               </div>
-              <div>
-                <select name="" id="">
-                  <option value=""></option>
-                </select>
-              </div>
-              <div>
-                <h1>Contact s</h1>
-              </div>
-            </div>
           </div>
         </div>
       </div>
