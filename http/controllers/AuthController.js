cd8ff7f31f954f7faa12a0bc2e893ed1ba5394b9
Daniel Panyin Mensah <78688545+Daniel024419@users.noreply.github.com>
2023-11-07 12:19:36 +0100

    UPDATED THE AUTH

--- http/controllers/AuthController.js
+++ http/controllers/AuthController.js
@@ -205,7 +205,7 @@ const data_token = jwt.sign({userData , message:"Authenticated as "+email ,token
 
 
 // res.redirect(`${process.env.SERVER_APP_URL_REDIRECT_CLIENT}/auth/google/callback?data_token=${data_token}`);
-res.redirect(`${process.env.SERVER_APP_URL_REDIRECT_PRO}/auth/google/callback?data_token=${data_token}`);
+res.redirect(`${process.env.SERVER_APP_URL_REDIRECT_CLIENT_PRO}/auth/google/callback?data_token=${data_token}`);
 
 }else{
 res.status(200).json({message : "User not found.. ",statusCode:404});
