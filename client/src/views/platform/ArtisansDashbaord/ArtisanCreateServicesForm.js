afbf7387398b869c3fa694d8ffead6515a201acd
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2023-11-09 10:59:11 +0100

    day 4

---
+++ client/src/views/platform/ArtisansDashbaord/ArtisanCreateServicesForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ArtisandashboardTopNavBar from "./includes/ArtisandashboardTopNavBar"
+import CreateServiceForm from './CreateServiceForm';
+
+const ArtisanCreateServicesForm = () => {
+  return (
+    <div>
+       <ArtisandashboardTopNavBar />
+      <h1 className="artitablehome">     <span className="title-top-platform"><i className="fas fa-tachometer"></i> Dashbaord {'>'} <i className="fas fa-home"></i>  Create</span>
+      </h1>
+      < CreateServiceForm />
+    </div>
+  )
+  }
+export default ArtisanCreateServicesForm;
---
+++ client/src/views/platform/ArtisansDashbaord/CreateServiceForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateServiceForm() {
+  return (
+    <div>
+        <h1>Form</h1>
+    </div>
+  )
+}
+
+export default CreateServiceForm
